3cd0373531f177e1b43a79ffb4ffb1afb9ee393f
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-06-28 16:33:50 +0300

    Initial multiple sidebars setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,55 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import starlightUtils from '@lorenzo_lewis/starlight-utils';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
+			title: 'Crowdin',
+			logo: {
+				replacesTitle: true,
+				light: './src/assets/logo/dark.svg',
+				dark: './src/assets/logo/light.svg',
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+					label: 'Crowdin',
+					autogenerate: { directory: 'crowdin' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Enterprise',
+					autogenerate: { directory: 'enterprise' },
 				},
+				{
+					label: 'Developer',
+					autogenerate: { directory: 'developer' },
+				},
+				{
+					label: "leading",
+					items: [
+						{ label: "Crowdin KB", link: "/crowdin" },
+						{ label: "Enterprise KB", link: "/enterprise" },
+						{ label: "Developer Portal", link: "/developer" },
+					],
+				}
 			],
 			customCss: ['./src/tailwind.css'],
+			pagination: false,
+			plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "dropdown",
+          },
+					navLinks: {
+						leading: { useSidebarLabelled: "leading" },
+					}
+        })
+      ],
 		}),
 		tailwind({ applyBaseStyles: false }),
 	],
---
+++ src/content/common/common.md
@@ -0,0 +1,7 @@
+## Creating a project
+
+To create a new project, you can use the `create` command
+
+## Translation process
+
+The translation process is done in two steps
